667faec26f2ec79d93daf398eb69a5b135c8d4e5
cisse410 <dramisse90@gmail.com>
2024-12-15 23:38:08 +0000

    recuperation des donnees depuis le controller

--- src/main/java/sn/cisse410/controller/HomeController.java
+++ src/main/java/sn/cisse410/controller/HomeController.java
@@ -1,17 +1,28 @@
 package sn.cisse410.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import sn.cisse410.model.Customer;
+import sn.cisse410.service.CustomerService;
+
 @Controller
 public class HomeController {
 
+    private final CustomerService customerService;
+
+    public HomeController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping("/")
     public String home(@RequestParam(defaultValue = "") String name, Model model) {
-        String greeting = "Bonjour, " + name;
-        model.addAttribute("message", greeting);
+        List<Customer> customers = customerService.getAllCustomers();
+        model.addAttribute("customers", customers);
         return "home";
     }
 

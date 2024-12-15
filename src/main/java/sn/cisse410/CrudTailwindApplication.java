8d3e55c65c8c2f58494dd54940888b0d22e0c7cb
cisse410 <dramisse90@gmail.com>
2024-12-15 22:24:59 +0000

    initial commit

---
+++ src/main/java/sn/cisse410/CrudTailwindApplication.java
@@ -0,0 +1,13 @@
+package sn.cisse410;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTailwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTailwindApplication.class, args);
+	}
+
+}

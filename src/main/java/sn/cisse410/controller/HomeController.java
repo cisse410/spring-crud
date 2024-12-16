0d9c04ab7c5f67cbea54711865226076b5861a0f
cisse410 <dramisse90@gmail.com>
2024-12-16 19:30:59 +0000

    ajout de client par le bien d'un formulaire

--- src/main/java/sn/cisse410/controller/HomeController.java
+++ src/main/java/sn/cisse410/controller/HomeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import sn.cisse410.model.Customer;
@@ -26,4 +28,16 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/create")
+    public String create(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Customer customer, Model model) {
+        customerService.saveCustomer(customer);
+        return "redirect:/";
+    }
 }

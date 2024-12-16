234bac1a52474d031a6cd0de8218ef8f65804c2d
cisse410 <dramisse90@gmail.com>
2024-12-16 22:12:55 +0000

    affichage details, modidification et suppression

--- src/main/java/sn/cisse410/controller/HomeController.java
+++ src/main/java/sn/cisse410/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -46,6 +47,26 @@ public class HomeController {
         }
         customerService.saveCustomer(customer);
         redirectAttributes.addFlashAttribute("message", "Client ajouté avec success!");
+        return "redirect:/";
+    }
+
+    @GetMapping("/customer/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        customerService.getCustomerById(id).ifPresent(customer -> model.addAttribute("customer", customer));
+        return "show";
+    }
+
+    @GetMapping("/customer/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Customer customer = customerService.getCustomerById(id).orElse(null);
+        model.addAttribute("customer", customer);
+        return "create";
+    }
+
+    @GetMapping("/customer/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        customerService.deleteCustomer(id);
+        redirectAttributes.addFlashAttribute("message", "Client supprimé avec success!");
         return "redirect:/";
     }
 }
